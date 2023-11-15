d2d209af566ac593b57aeab19d97b42de5b41018
reck_wang <reck_wang@wxp.com>
2023-11-16 00:10:47 +0800

    complete question 123, 188

---
+++ 123. Best Time to Buy and Sell Stock III/main.go
@@ -0,0 +1,30 @@
+package main
+
+func maxProfit(prices []int) int {
+	k := 2
+	n := len(prices)
+	dp := make([][][2]int, n)
+	for i := range dp {
+		dp[i] = make([][2]int, k+1)
+	}
+
+	for i := 0; i < k+1; i++ {
+		dp[0][i][1] = -prices[0]
+	}
+
+	for i := 1; i < n; i++ {
+		for j := 1; j < k+1; j++ {
+			dp[i][j][0] = max(dp[i-1][j][0], dp[i-1][j][1]+prices[i])
+			dp[i][j][1] = max(dp[i-1][j][1], dp[i-1][j-1][0]-prices[i])
+		}
+	}
+
+	return dp[n-1][k][0]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
